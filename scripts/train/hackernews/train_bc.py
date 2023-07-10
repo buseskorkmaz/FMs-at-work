5c7c351141a0d3c16f57238acd6197664ed9fbbd
buseskorkmaz <buseskorkmaz@gmail.com>
2023-07-10 14:09:17 -0400

    develop hackkernews environment

---
+++ scripts/train/hackernews/train_bc.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../'))
+import hydra
+from omegaconf import DictConfig, OmegaConf
+# import bias.load_objects
+from train.bc_train_loop import train
+
+@hydra.main(config_path="../../../config/hackernews", config_name="train_bc")
+def main(cfg : DictConfig):
+    cfg = OmegaConf.to_container(cfg)
+    train(cfg)
+
+if __name__ == "__main__":
+    main()
